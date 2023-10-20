2589f043faa1e68a258ebee69b015d390dbf8a51
Sam <100703206+SamChrp@users.noreply.github.com>
2023-10-20 12:22:47 +0200

    wip : on off

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,8 @@
   const shellyParameters = ref("");
   const response = ref("");
   const togglePower = ref("");
+  const isOn = ref("");
+  const param = ref("");
 
   onMounted(async () => {
     const response = await axios.post(
@@ -20,7 +22,9 @@
         }
     );
 
-    shellyParameters.value = response.data.data.device_status
+    shellyParameters.value = await response.data.data.device_status
+    isOn.value = await response.data.data.device_status.relays[0].ison
+
     console.log(shellyParameters)
   })
 
@@ -38,13 +42,41 @@
         }
     );
 
+    if (isOn.value) {
+      let param = "on"
+    } else {
+      let param = "off"
+    }
+
+    let requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    // axios.post(
+    //     `https://shelly-86-eu.shelly.cloud/relay/0?turn=${param}`, requestOptions,
+    //     {
+    //       "id": "80646F827174",
+    //       "auth_key": "MWRmYzM2dWlkE62C6C4C76F817CE0A3D2902F5B5D4C115E49B28CF8539114D9246505DE5D368D560D06020A92480"
+    //     },
+    //     {
+    //       headers: {
+    //         'Content-Type': 'application/x-www-form-urlencoded',
+    //       }
+    //     }
+    // );
+
+    fetch(`http://192.168.1.100/relay/0?turn=${param}`, requestOptions)
+        .then(response => response.text())
+        .catch(error => console.log('error', error));
+
     switchPower.value = response.data.data.device_status
 
   }
 </script>
 
 <template>
-<p>{{ shellyParameters }}</p>
+  <button @click="switchPower()">{{ isOn }}</button>
+
   <div v-for="(data, key) in shellyParameters">
     <p>{{ key }} : {{ data }}</p>
     <hr>
